19777a3bc8794b037dd85b5f3ca6e36a2c48649d
Shiven Shekar <shivenshekar01@gmail.com>
2024-12-31 03:20:01 -0800

    Created a basic wallet

---
+++ generate-wallet.js
@@ -0,0 +1,16 @@
+import { readFileSync, writeFileSync } from 'fs';
+import EC from 'elliptic';
+
+const ec = new EC.ec('p192');
+const keyPair = ec.genKeyPair();
+const publicKey = keyPair.getPublic('hex');
+
+const newWalletName = process.argv[2];
+const walletsFile = readFileSync('./wallets.json');
+let wallets = JSON.parse(walletsFile);
+
+if (!wallets.hasOwnProperty(newWalletName)) {
+  wallets[newWalletName] = publicKey;
+  wallets = JSON.stringify(wallets, null, 2);
+  writeFileSync('./wallets.json', wallets);
+}
---
+++ get-address.js
@@ -0,0 +1,6 @@
+import { getWalletAddressFromName } from './blockchain-helpers.js';
+
+const addressName = process.argv[2];
+const address = getWalletAddressFromName(addressName);
+
+console.log(`The public address for ${addressName} is ${address}`);
